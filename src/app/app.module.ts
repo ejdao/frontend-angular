a684916d0dcde3d05c691ccdbf258bcbcaf79d6c
ejdao <ejdao2019@hotmail.com>
2022-07-19 09:42:40 -0500

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
