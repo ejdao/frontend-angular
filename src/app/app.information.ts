ce73696812d3f8e92a3e1fbf9d298119ac8872de
ejdao <ejdao2019@hotmail.com>
2022-07-20 17:59:11 -0500

    [v0.0.2] Budget initial updated

--- src/app/app.information.ts
+++ src/app/app.information.ts
@@ -8,7 +8,7 @@ export interface appInfoI {
 
 export const appInfo = {
   name: 'Frontend Angular',
-  version: '0.0.1',
+  version: '0.0.2',
   released: '2022.07.20',
   company: 'Ejdao Develoment',
   creation: '2022',
